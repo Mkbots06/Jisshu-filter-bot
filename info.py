5b4adca5b43bc297a562c1297e694a48a51b6ae1
Mkbots06 <mkbotsnetwork01@gmail.com>
2025-03-05 15:53:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 USERNAME = environ.get('USERNAME', "https://t.me/mkadmin_sir") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/mkmovies_hub')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', 'https://t.me/mk_movie_request').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002336864238').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://usucbuy1:asYg3BEiN144YVo0@cluster0.uh1vn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
